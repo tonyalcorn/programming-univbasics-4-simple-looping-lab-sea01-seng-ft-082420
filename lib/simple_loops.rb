1b57d909c7062e3a9a06b888750ef04aa6ed19df
Anthony Alcorn <xmastony@gmail.com>
2020-08-14 03:10:57 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,9 +1,5 @@
 def loop_message_five_times(message)
-  puts message
-  puts message
-  puts message
-  puts message
-  puts message
+  puts message(5)
 end
 
 def loop_message_n_times(message, number)
