985a4d18ecfb6e844c6813e12a3ec04445cb1810
Anthony Alcorn <xmastony@gmail.com>
2020-08-14 03:10:05 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -14,5 +14,14 @@ def loop_message_n_times(message, number)
   end
 end
 
+def output_array(array)
+  count = 0 
+  while count < array.length do
+    puts array[count]
+    count += 1 
+  end
+end
+
+
 
 
